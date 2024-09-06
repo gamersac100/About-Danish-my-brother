5cd415ae1cf4f8e9f945190acb27b20306a8fd70
gamersac100 <160509911+gamersac100@users.noreply.github.com>
2024-09-06 23:19:26 +0500

    brother
    brother

---
+++ About danish.cmd
@@ -0,0 +1,9 @@
+@echo off
+color 2
+set /p="About Danish my brother"
+set /p="he is very bad boy"
+set /p="he look likes a cow"
+set /p="he never bath and he is very smelly"
+set /p="bye if you see him everywhere beat him"
+
+exit 
